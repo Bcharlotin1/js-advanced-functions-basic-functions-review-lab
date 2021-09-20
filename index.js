42a83c666b554e0de7b052d7893d1fb8d4861255
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-20 13:39:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+// function wrapAdjective(){
+//     return function(param = "special") {
+
+//     }
+// }
+
+function saturdayFun(activity = 'roller-skate' ) {
+    let currentActivity = activity
+    return currentActivity
+    
+}
