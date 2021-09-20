f4cd8dfdc2a6629af01e2805d2844425b2ff4ae0
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-20 14:06:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // Your code here
-// function wrapAdjective(){
-//     return function(param = "special") {
+function wrapAdjective(params ="*"){
+    return function(param = "special") {
+        return `You are ${params}${param}${params}!`
+    }
 
-//     }
-// }
+}
+
 
 function saturdayFun(activity = 'roller-skate' ) {
-    let currentActivity = activity
+    let currentActivity = `This Saturday, I want to ${activity}!`
     return currentActivity
+    
+}
+
+function mondayWork(activity = 'go to the office') {
+    let currentActivity = `This Monday, I will ${activity}.`
+    return currentActivity
+}
+
+function calculato(params) {
     
 }
